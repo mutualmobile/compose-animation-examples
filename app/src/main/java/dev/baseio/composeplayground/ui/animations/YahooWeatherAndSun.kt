a889022a189f2ada7c474a3a910a39946aae5cb0
Anmol Verma <anmol.verma4@gmail.com>
2022-03-11 13:52:55 +0530

    WIP for sun and moon, yahoo anim

--- app/src/main/java/dev/baseio/composeplayground/ui/animations/YahooWeatherAndSun.kt
+++ app/src/main/java/dev/baseio/composeplayground/ui/animations/YahooWeatherAndSun.kt
@@ -1,16 +1,21 @@
 package dev.baseio.composeplayground.ui.animations
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -23,6 +28,31 @@ import dev.baseio.composeplayground.ui.theme.Typography
  */
 @Composable
 fun YahooWeatherAndSun(modifier: Modifier) {
+
+  var sunMovesAlong by remember {
+    mutableStateOf(false)
+  }
+
+  var maskedRectScaledAlong by remember {
+    mutableStateOf(false)
+  }
+
+  val angleRotation by animateFloatAsState(
+    targetValue = if (sunMovesAlong) 145f else 20f,
+    animationSpec = tween(durationMillis = 5000, delayMillis = 2000, easing = LinearEasing)
+  )
+
+  val scaleRect by animateFloatAsState(
+    targetValue = if (maskedRectScaledAlong) 0.9f else 0f,
+    animationSpec = tween(durationMillis = 5000, delayMillis = 2000, easing = LinearEasing)
+  )
+
+  LaunchedEffect(key1 = true) {
+    sunMovesAlong = !sunMovesAlong
+    maskedRectScaledAlong = !maskedRectScaledAlong
+  }
+
+
   Box(
     modifier
       .background(Color(0xff112937))
@@ -36,30 +66,56 @@ fun YahooWeatherAndSun(modifier: Modifier) {
       verticalArrangement = Arrangement.SpaceAround,
       horizontalAlignment = Alignment.CenterHorizontally
     ) {
-      Text(
-        text = stringResource(id = R.string.sun_moon),
-        style = Typography.h5.copy(color = Color.White),
-      )
-
-
       Box() {
-        PathArcCanvas()
+        Text(
+          text = stringResource(id = R.string.sun_moon),
+          style = Typography.h5.copy(color = Color.White),
+        )
         Text(
           text = "5:44 AM",
           style = Typography.caption.copy(color = Color.White),
-          modifier = Modifier.offset(y = 160.dp, x = (-20).dp)
+          modifier = Modifier.offset(y = 280.dp, x = (-120).dp)
         )
         Text(
           text = "20:01 PM",
           style = Typography.caption.copy(color = Color.White),
-          modifier = Modifier.offset(y = 160.dp, x = (270).dp)
+          modifier = Modifier.offset(y = 280.dp, x = (190).dp)
         )
+      }
+
+      Box {
+        PathArcCanvas(Modifier.align(Alignment.Center))
+
         Icon(
           painter = painterResource(id = R.drawable.ic_sun),
           contentDescription = null,
-          modifier = Modifier.offset(y = 130.dp, x = (-10).dp),
+          modifier = Modifier.offset(x=26.dp,y=146.dp)
+            .rotate(angleRotation),
           tint = Color(0xfff9d71c)
         )
+
+        Box(
+          Modifier
+            .fillMaxWidth(fraction = 0.98f)
+            .align(Alignment.Center)
+            .height(1.dp)
+            .background(Color.White.copy(0.5f))
+            .offset(y = 160.dp)
+        )
+
+
+
+        Box(
+          Modifier
+            .graphicsLayer(shape = CircleShape)
+            .align(Alignment.Center)
+            .size(320.dp)
+        ) {
+          ContainedCanvas(scaleRect)
+        }
+
+
+
       }
 
       Box(
@@ -74,8 +130,26 @@ fun YahooWeatherAndSun(modifier: Modifier) {
 }
 
 @Composable
-private fun PathArcCanvas() {
-  Canvas(modifier = Modifier.size(300.dp), onDraw = {
+private fun ContainedCanvas(scaleRect: Float) {
+  Canvas(
+    modifier = Modifier
+      .width(300.dp)
+      .height(160.dp)
+      .graphicsLayer(
+        transformOrigin = TransformOrigin(0f, 0f),
+        scaleX = scaleRect, scaleY = 1f
+      ),
+    onDraw = {
+      drawRect(
+        color = Color(0xfff9d71c),
+        alpha = 0.1f,
+      )
+    })
+}
+
+@Composable
+private fun PathArcCanvas(modifier: Modifier) {
+  Canvas(modifier = modifier.size(320.dp), onDraw = {
     drawArc(
       color = Color(0xfff9d71c),
       style = Stroke(
