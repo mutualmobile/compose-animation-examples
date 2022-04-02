57ba1231f567982573a162e5ab34dc5eeb9e0756
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2022-04-02 17:06:23 +0530

    Basic Circle Animation

--- app/src/main/java/dev/baseio/composeplayground/ui/animations/BreatheAnimation.kt
+++ app/src/main/java/dev/baseio/composeplayground/ui/animations/BreatheAnimation.kt
@@ -12,8 +12,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import dev.baseio.composeplayground.R
 
@@ -26,14 +28,15 @@ fun BreatheAnimation(modifier: Modifier = Modifier) {
       .background(Color.Black)
   ) {
 
-    Box(
-      modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Black)
+    Column(modifier = Modifier
+      .fillMaxWidth()
+      .align(Alignment.Center),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-
-
+      AnimatedCircles(6)
+      Spacer(modifier = Modifier.padding(24.dp))
+      AnimatedText()
     }
 
     Box(
@@ -48,4 +51,73 @@ fun BreatheAnimation(modifier: Modifier = Modifier) {
           .background(Color.LightGray))
     }
   }
+}
+
+@Composable
+fun AnimatedCircles(numCircles :Int){
+  Box(modifier = Modifier.fillMaxWidth()) {
+    for (circleNumber in 0 until numCircles){
+      AnimatedCircle(
+        modifier = Modifier
+          .padding(16.dp)
+          .align(Alignment.Center),
+        rotationQuota = 360f/numCircles,
+        circleNumber = circleNumber
+      )
+    }
+  }
+}
+
+@Composable
+fun AnimatedCircle(
+  modifier: Modifier,
+  rotationQuota : Float,
+  circleNumber : Int
+){
+
+  val circleRotation = remember { Animatable(0f) }
+  val circleScale = remember { Animatable(0.1f) }
+
+  LaunchedEffect(circleRotation){
+    circleRotation.animateTo(
+      targetValue = rotationQuota * circleNumber,
+      animationSpec = infiniteRepeatable(
+        tween(5000, easing = LinearEasing),
+        repeatMode = RepeatMode.Reverse
+      )
+    )
+  }
+
+  LaunchedEffect(circleScale){
+    circleScale.animateTo(
+      targetValue = 1f,
+      animationSpec = infiniteRepeatable(
+        tween(5000, easing = LinearEasing),
+        repeatMode = RepeatMode.Reverse
+      )
+    )
+  }
+
+  Column(modifier = modifier
+    .rotate(circleRotation.value)
+    .scale(circleScale.value),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Canvas(modifier = Modifier
+      .size(120.dp)
+      .alpha(0.3f)){
+      drawCircle(color = Color.Cyan)
+    }
+    Canvas(modifier = Modifier
+      .size(125.dp)
+      .alpha(0f)){
+      drawRect(color = Color.Red)
+    }
+  }
+}
+
+@Composable
+fun AnimatedText() {
+  Text(text = "Breathe In...", style = TextStyle(color = Color.LightGray))
 }
