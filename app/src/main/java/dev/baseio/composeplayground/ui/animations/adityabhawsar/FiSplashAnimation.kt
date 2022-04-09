6747c96542ffe1a7103db56e6fbaf7246368056c
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2022-04-09 09:42:26 +0530

    Added circle

--- app/src/main/java/dev/baseio/composeplayground/ui/animations/adityabhawsar/FiSplashAnimation.kt
+++ app/src/main/java/dev/baseio/composeplayground/ui/animations/adityabhawsar/FiSplashAnimation.kt
@@ -1,10 +1,12 @@
 package dev.baseio.composeplayground.ui.animations.adityabhawsar
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.baseio.composeplayground.contributors.AdityaBhawsar
@@ -18,7 +20,15 @@ fun FiSplashAnimation() {
       .background(Color.White)
   ) {
 
-
+    Canvas(
+      modifier = Modifier
+        .alpha(1f)
+        .size(16.dp)
+        .align(Alignment.Center),
+      onDraw = {
+        drawCircle(color = Color(0xFF00B899))
+      }
+    )
 
     Box(
       modifier = Modifier
