3a17d8eca0c20bbd0ca82b680a5dc83ebbf1f0de
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2022-04-14 23:07:59 +0530

    Created a swipeable slider

--- app/src/main/java/dev/baseio/composeplayground/ui/animations/adityabhawsar/AlarmSliderAnimation.kt
+++ app/src/main/java/dev/baseio/composeplayground/ui/animations/adityabhawsar/AlarmSliderAnimation.kt
@@ -9,11 +9,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.baseio.composeplayground.R
 import dev.baseio.composeplayground.contributors.AdityaBhawsar
 import kotlin.math.roundToInt
@@ -21,15 +22,20 @@ import kotlin.math.roundToInt
 @Composable
 fun AlarmSliderAnimation() {
 
-  Box(modifier = Modifier
-    .fillMaxSize()
-    .background(Color.Black)){
+  Box(
+    modifier = Modifier
+      .fillMaxSize()
+      .background(Color(0xff1f231f))
+  ) {
 
-    Box(modifier = Modifier
-      .fillMaxWidth()
-      .align(Alignment.Center)){
+    Box(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp)
+        .align(Alignment.Center)
+    )
+    {
       AlarmSlider()
-
     }
 
     Box(
@@ -41,7 +47,8 @@ fun AlarmSliderAnimation() {
       AdityaBhawsar(
         Modifier
           .align(Alignment.Center)
-          .background(Color.LightGray))
+          .background(Color.LightGray)
+      )
     }
   }
 
@@ -52,10 +59,14 @@ fun AlarmSliderAnimation() {
 fun AlarmSlider() {
   var lastState = 0f
 
-  BoxWithConstraints(modifier = Modifier.fillMaxWidth().height(50.dp)) {
-    val swipeableState = rememberSwipeableState(0f){ state ->
+  BoxWithConstraints(
+    modifier = Modifier
+      .fillMaxWidth()
+      .height(80.dp)
+  ) {
+    val swipeableState = rememberSwipeableState(0.5f) { state ->
       if (state == 1f) {
-         //Todo  on Swipe End
+        //Todo  on Swipe End
       } else {
         //Todo on Swipe Start
       }
@@ -64,43 +75,75 @@ fun AlarmSlider() {
       true
     }
 
-    val swipeEnd: Float = constraints.maxWidth - (constraints.maxHeight * 1.5f)
+    val swipeCenter: Float = (constraints.maxWidth / 2).toFloat() - ((constraints.maxHeight * 1.35f) / 2)
+    val swipeRight: Float = constraints.maxWidth - (constraints.maxHeight * 1.35f)
+    val swipeLeft = 0f
+
     val anchors = mapOf(
-      0f to 0f,
-      swipeEnd to 1f
+      swipeLeft to 0f,
+      swipeCenter to 0.5f,
+      swipeRight to 1f
     )
 
     Box(
       modifier = Modifier
         .fillMaxWidth()
-        .clip(RoundedCornerShape(8.dp))
-        .background(Color.White)
+        .clip(RoundedCornerShape(40.dp))
+        .background(Color(0xff353a3d))
         .swipeable(
           state = swipeableState,
           anchors = anchors,
-          thresholds = { _, _ -> FractionalThreshold(0.5f) },
+          thresholds = { _, _ -> FractionalThreshold(0.7f) },
           orientation = Orientation.Horizontal
         )
-    ){
+    ) {
+
+      Text(
+        modifier = Modifier.align(Alignment.CenterStart).padding(16.dp),
+        text = "Snooze",
+        style = TextStyle(
+          color = Color.White,
+          fontSize = 18.sp
+        )
+      )
+
+      Text(
+        modifier = Modifier.align(Alignment.CenterEnd).padding(16.dp),
+        text = "Stop",
+        style = TextStyle(
+          color = Color.White,
+          fontSize = 18.sp
+        )
+      )
+
+      Box(
+        modifier = Modifier
+          .fillMaxHeight()
+          .aspectRatio(1.35f, true)
+          .padding(8.dp)
+          .clip(RoundedCornerShape(40.dp))
+          .background(Color(0xff626972))
+          .align(Alignment.Center)
+      )
 
       Box(
         modifier = Modifier
           .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
           .fillMaxHeight()
-          .aspectRatio(1.5f, true)
-          .padding(2.dp)
-          .shadow(6.dp)
-          .background(Color.Yellow)
-          .align(Alignment.CenterStart)
-          .clip(RoundedCornerShape(8.dp))
-      ){
+          .aspectRatio(1.35f, true)
+          .padding(8.dp)
+          .clip(RoundedCornerShape(40.dp))
+          .background(Color(0xffd6e8fb))
+      ) {
         Icon(
-          painter = painterResource(id = R.drawable.ic_mic_white_40),
-          modifier = Modifier.size(28.dp).align(Alignment.Center),
-          contentDescription = ""
+          painter = painterResource(id = R.drawable.ic_alarm),
+          modifier = Modifier
+            .size(24.dp)
+            .align(Alignment.Center),
+          contentDescription = "",
+          tint = Color.Black
         )
       }
-
     }
   }
 }
